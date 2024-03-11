2ee2ad85f6ca0ab00847f9340a929815351aeb3d
Ubuntu_64_forOS_shenhaomin <1917325451@qq.com>
2024-03-11 14:01:25 +0800

    章节6-b

--- myos/kernel/main.c
+++ myos/kernel/main.c
@@ -1,17 +1,6 @@
 #include "print.h"
-void main(void)
-{
-    put_char('k');
-    put_char('e');
-    put_char('r');
-    put_char('n');
-    put_char('e');
-    put_char('l');
-    put_char('\n');
-    put_char('1');
-    put_char('2');
-    put_char('\b');
-    put_char('3');
-    while(1);
-    
+void main(void) {
+   put_str("I am kernel\n");
+   while(1);
 }
+
